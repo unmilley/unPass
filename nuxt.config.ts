ce6051404c722e2867e379e510539566551ab31a
unmilley <1337@milley.uno>
2025-10-17 10:27:44 +0500

    feat: add @tauri-apps/api & vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['@nuxt/icon'],
+	modules: ['@nuxt/icon', '@vueuse/nuxt'],
 
 	css: ['~/assets/css/main.css'],
 
