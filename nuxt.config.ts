8dc77f891a7e7a3cdd481923ae32319ec7330020
unmilley <1337@milley.uno>
2025-10-17 08:36:21 +0500

    feat: add tailwind & daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	modules: ['@nuxt/icon'],
 
+	css: ['~/assets/css/main.css'],
+
 	ssr: false,
 	vite: {
+		plugins: [tailwindcss()],
 		clearScreen: false,
 		envPrefix: ['VITE_', 'TAURI_'],
 		server: { strictPort: true },
