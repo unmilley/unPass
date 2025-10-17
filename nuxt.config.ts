0d4b287549c21688b70ef682f74c05e2105a0739
unmilley <1337@milley.uno>
2025-10-17 12:18:48 +0500

    feat: add optimize for icons & color-theme module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,25 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['@nuxt/icon', '@vueuse/nuxt'],
+	modules: ['@nuxt/icon', '@vueuse/nuxt', '@nuxtjs/color-mode'],
 
 	css: ['~/assets/css/main.css'],
+
+	icon: {
+		mode: 'svg',
+		collections: ['bx'],
+		provider: 'iconify',
+		size: '1.25rem',
+	},
+
+	colorMode: {
+		preference: 'system',
+		fallback: 'dark',
+		classSuffix: '',
+		dataValue: 'theme',
+		storageKey: 'theme',
+		storage: 'cookie',
+	},
 
 	ssr: false,
 	vite: {
