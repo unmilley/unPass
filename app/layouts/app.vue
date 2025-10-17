52ce22e70daac6c4867e77a9636cc6b2d079b4ee
unmilley <1337@milley.uno>
2025-10-17 10:29:42 +0500

    feat: add structure for pages and layouts

---
+++ app/layouts/app.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<main :class="{ 'mt-[2.25rem]': $isTauri }">
+			<slot />
+		</main>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1 class="text-3xl font-bold underline">Index!</h1>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
