5f5c5ea60589b14e5cd174d36bd961ca700b5c92
unmilley <1337@milley.uno>
2025-10-19 00:44:00 +0500

    feat(tauri+nuxt): add icons & add pwa webmanifest to head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,18 @@ export default defineNuxtConfig({
 		storageKey: 'theme',
 		storage: 'cookie',
 	},
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', type: 'image/png', href: '/pwa/favicon-96x96.png', sizes: '96x96' },
+				{ rel: 'icon', type: 'image/svg+xml', href: '/pwa/favicon.svg' },
+				{ rel: 'shortcut icon', href: '/pwa/favicon.ico' },
+				{ rel: 'apple-touch-icon', sizes: '180x180', href: '/pwa/apple-touch-icon.png' },
+				{ rel: 'manifest', href: '/pwa/app.webmanifest', crossorigin: 'use-credentials' },
+			],
+			meta: [{ name: 'apple-mobile-web-app-title', content: 'unPass' }],
+		},
+	},
 
 	ssr: false,
 	vite: {
