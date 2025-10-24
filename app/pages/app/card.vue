31e0b940bc8d0de8d21209bb6df0779ac6f64ca3
unmilley <1337@milley.uno>
2025-10-24 08:37:37 +0500

    feat: add empty pages

---
+++ app/pages/app/card.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h1 class="text-3xl font-bold underline">Card!</h1>
+	</div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({ layout: 'app' })
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<h1 class="text-3xl font-bold underline">Index!</h1>
+		<nuxt-link to="/app/sign-in" class="btn">To app</nuxt-link>
 	</div>
 </template>
 
